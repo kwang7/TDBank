46ea69f882d591fe313ebfdaa2917c0ad43cdb49
Kelly Wang <kwang7@stuy.edu>
2017-10-16 23:10:06 -0400

    donezoed

--- stu_mean.py
+++ stu_mean.py
@@ -39,12 +39,46 @@ for row in coursez:
     iD = row['id']
     c.execute("INSERT INTO courses VALUES (?,?,?)",( code , mark, iD ))
 
+'''
 q = "SELECT name, peeps.id , mark FROM peeps, courses WHERE peeps.id = courses.id;"
 foo = c.execute(q)
 print foo
 
 for bar in foo:
     print bar
+'''
+#---------------------------------------------------------------------------------------------
+
+grades = {} #store students info here
+
+#select each student's grades and add it to their corresponding key in the dictionary
+coolGrades = c.execute("SELECT name, peeps.id, mark FROM peeps, courses WHERE peeps.id = courses.id;")
+for row in coolGrades:
+    #print row
+    peep = row[0]
+    idee = row [1]
+    grade = row[2]
+    #add grades to dictionary, key is student id
+    if idee not in grades:
+        grades[idee] = [peep]
+        grades[idee].append(grade)
+    else:
+        grades[idee].append(grade)
+
+#calculate avg
+for key in grades:
+    coolgrades = grades[key][1:len(grades[key])]
+    avg = sum(coolgrades) / len (coolgrades)
+    grades[key].append(avg)
+    
+#display everything
+for key in grades:
+    #first item in the dict is name
+    print 'NAME: ' , grades[key][0]
+    print 'ID: ', key
+    #last item in the dict is avg
+    print 'AVG: ' ,grades[key][len(grades[key])-1]
+
 
 db.commit() #save changes
 db.close()  #close database
