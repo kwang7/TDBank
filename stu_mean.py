f28457116b9bb18383b68ce16ba7c001c7d4997e
Kelly Wang <kwang7@stuy.edu>
2017-10-17 22:19:43 -0400

    I BROKE EVERYTHING

--- stu_mean.py
+++ stu_mean.py
@@ -1,5 +1,5 @@
 '''
-Kelly Wang and jaspercheiugn
+Kelly Wang and jasper cheung
 SoftDev1 pd7
 HW10--Average
 2017-10-17
@@ -37,8 +37,7 @@ for row in coursez:
     code = row['code']
     mark = row['mark']
     iD = row['id']
-    c.execute("INSERT INTO courses VALUES (?,?,?)",( code , mark, iD ))
-
+    qq =c.execute("INSERT INTO courses VALUES (?,?,?)",( code , mark, iD ))
 '''
 
 '''
@@ -47,40 +46,64 @@ for row in coursez:
 grades = {} #store students info here
 
 #select each student's grades and add it to their corresponding key in the dictionary
-coolGrades = c.execute("SELECT name, peeps.id, mark FROM peeps, courses WHERE peeps.id = courses.id;")
-for row in coolGrades:
-    print row
-    peep = row[0]
-    idee = row [1]
-    grade = row[2]
-    #add grades to dictionary, key is student id
-    if idee not in grades:
-        grades[idee] = [peep]
-        grades[idee].append(grade)
-    else:
-        grades[idee].append(grade)
+def makeDict():
+    coolGrades = c.execute("SELECT name, peeps.id, mark FROM peeps, courses WHERE peeps.id = courses.id;")
+    for row in coolGrades:
+        print row
+        peep = row[0]
+        idee = row [1]
+        grade = row[2]
+        if idee not in grades:
+            grades[idee] = [peep]
+            grades[idee].append(grade)
+        else:
+            grades[idee].append(grade)
+        return grades 
+makeDict()
+
+
 '''
 for key in grades:
     print key
     print grades[key]
 '''
-        
+
 #calculate avg
 def calcAvg(key):
     coolgrades = grades[key][1:len(grades[key])]
     #print coolgrades
     avg = ( sum(coolgrades) / float(len(coolgrades))) 
     return avg
-    
+
 
 #create table of ids and associated averages
-c.execute("CREATE TABLE peeps_avg ( id INTEGER, avg NUMERIC )")
-for key in grades:
-    c.execute("INSERT INTO peeps_avg VALUES ( ? , ? )" , ( key, calcAvg(key)))
+q = "CREATE TABLE peeps_avg ( id INTEGER, avg NUMERIC )"
+pp = c.execute(q)
+def addStuff():
+    for key in grades:
+        #print key
+        #print calcAvg(key)
+        c.execute("INSERT INTO peeps_avg VALUES ( ? , ? )" , ( key, calcAvg(key)))
+    return grades
+addStuff()
+'''    
+def addCourse( c0de , marK , iDee ):
+    #add info to courses
+    c.execute("INSERT INTO courses VALUES( ? , ? , ? )", (c0de,marK,iDee))
+    #add info to dictionary
+    grades[iDee].append(marK)
     
-def updateGrades():
-    
-        
+#addCourse( 'physics', 99, 10 )
+addCourse( 'calc', 23, 1 )
+addCourse( 'cycling', 17, 1 )
+'''
+'''
+def updateAvg(iDee):
+    newAvg = calcAvg(iDee);
+    c.execute("UPDATE peeps_avg SET avg = ? WHERE id = ? ",(newAvg,iDee))
+updateAvg(1);
+updateAvg(2);
+  '''  
 #display everything
 def displayEverything():
     for key in grades:
